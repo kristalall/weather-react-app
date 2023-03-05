58f75f3ced5fe8918ffb00d3a0a20245d0262275
kristalall <kristalall93@gmail.com>
2023-03-05 12:07:42 -0600

    Added link to open-source code

--- src/App.js
+++ src/App.js
@@ -5,6 +5,13 @@ function App() {
   return (
     <div className="App">
       <Weather />
+      <p>
+        View{" "}
+        <a href="https://github.com/kristalall/weather-react-app">
+          open-source code
+        </a>{" "}
+        on GitHub by Kristal All
+      </p>
     </div>
   );
 }
